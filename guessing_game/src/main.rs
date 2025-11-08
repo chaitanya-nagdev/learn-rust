2b279f494a6b6d88ffc63204fa0aac2d5eac0bd3
chaitanya <nagdev.chaitanya@gmail.com>
2025-11-08 18:00:22 +0530

    Finish chapter too with guessing game, with coloured text print

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,24 +1,40 @@
 use colored::*;
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!();
-    println!();
     println!("----------------------");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("{}", "Guess the number!".cyan().bold());
-    println!("{}", "Please input your guess.".cyan().bold());
 
-    let mut guess = String::new(); // creates a mutable variable with empty string binded a value. 
+    loop {
+        println!("{}", "\nPlease input your guess.".cyan().bold());
 
-    io::stdin() // returns a Type 'Stdin' used to handle input operation in a process
-        .read_line(&mut guess) // appends the user input
-        .expect("failed to read line");
+        let mut guess = String::new(); // creates a mutable variable with empty string binded a value. 
 
-    println!(
-        "\n{} {}",
-        "You guessesd".yellow().bold(),
-        guess.yellow().bold()
-    );
+        io::stdin() // returns a Type 'Stdin' used to handle input operation in a process
+            .read_line(&mut guess) // appends the user input
+            .expect("failed to read line");
+
+        // match expression on Result state
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue, // matches all values of err via _
+        };
+
+        println!("\n{} {}", "You guessesd".yellow().bold(), guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too small!".red().bold()),
+            Ordering::Greater => println!("{}", "Too big!".red().bold()),
+            Ordering::Equal => {
+                println!("{}", "You win!".green().bold());
+                break;
+            }
+        }
+    }
 }
 
 /*
@@ -45,11 +61,16 @@ fn main() {
     Result : As this is type, it has associated function with it. expect function if Result is 'Err'
             value then it will crash the program. If 'Ok' it will return the value 'Ok' is holding
             thus value can be used later.
+
 12) {} : empty brackets display the value in order they are defined in println()! macro.
 
 
 13) Importing crate : Add in 'dependency', bring in scope using 'use'
-
+14) Default type infered number is 'u32'
+15) We are 'shadowing' guess variable, thus reusing variable name rather than forcing us to create two new variable.
+    Used when one type needs to be converted to another type.
+16) trim() -> removes whitespace from start & end
+17) parse() -> convert string to other data type, type is defined via :u32 thus to variable we are assigning things to.
 
 ----------------------------
 */
